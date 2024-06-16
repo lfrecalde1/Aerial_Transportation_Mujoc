9e8dc0c98681ace9f612869efb4083e2d3306b62
fer <fernandorecalde@uti.edu.ec>
2024-06-15 22:49:08 -0500

    cpp mujoco

---
+++ launch/launcher.py
@@ -0,0 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    xml_file_name = "model/xml/balloons.xml"
+    xml_file = os.path.join(get_package_share_path("drone_mujoco"), xml_file_name)
+    print(xml_file)
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'model_path',
+            default_value=xml_file,
+            description='Path to the model XML file.'
+        ),
+        Node(
+            package='drone_mujoco',
+            executable='my_node',
+            name='simulate_node',
+            output='screen',
+            arguments=[LaunchConfiguration('model_path')],
+        )
+    ])
